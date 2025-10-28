61888c22af554daca0b92ae935c575ec1577dfd4
torbkle <torkle05553@stud.noroff.no>
2025-10-28 13:12:45 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,9 @@
-from supabase import create_client
 import datetime
+import streamlit as st
+from supabase import create_client
 
-SUPABASE_URL = "https://zewmjurylmyjweyqotpw.supabase.co"  # ← bytt ut med din faktiske URL
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpld21qdXJ5bG15andleXFvdHB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0MDAwOTYsImV4cCI6MjA3NDk3NjA5Nn0.4awJMuo5KHQM1-i0EaIZC0Gf8q9hQYwqRqZc10IWc2o"  # ← bytt ut med din faktiske nøkkel
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+# Opprett klient med verdier fra secrets.toml
+supabase = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
 
 def lagre_treningsøkt(bruker, aktivitet, varighet, distanse, kommentar):
     data = {
