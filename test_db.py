24c7f388309e53b07e4c2cb11d72e7b8335c7991
torbkle <torkle05553@stud.noroff.no>
2025-10-28 14:24:53 +0100

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,12 +1,1 @@
-import streamlit as st
-from db import lagre_treningsøkt
-
-def test_supabase():
-    response = lagre_treningsøkt(
-        bruker="Torbjørn",
-        aktivitet="Testøkt",
-        varighet=30,
-        distanse=5.0,
-        kommentar="Test fra Streamlit"
-    )
-    st.write("Respons:", response)
+Tømmer testfil
