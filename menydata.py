2fad517a4155a1eef2596d841b5eda0b2c59b0d7
torbkle <torkle05553@stud.noroff.no>
2025-10-29 13:46:13 +0100

    Create menydata.py

---
+++ menydata.py
@@ -0,0 +1,13 @@
+# menydata.py
+
+MENYVALG = [
+    {"navn": "Velkommen", "ikon": "🏠"},
+    {"navn": "Dagens plan", "ikon": "📅"},
+    {"navn": "Logg", "ikon": "📓"},
+    {"navn": "Fremgang", "ikon": "📈"},
+    {"navn": "Parvisning", "ikon": "🧑‍🤝‍🧑"},
+    {"navn": "Ukesmål", "ikon": "🎯"},
+    {"navn": "Ukentlig oppsummering", "ikon": "🗓️"},
+    {"navn": "Rediger mål", "ikon": "🛠️"},
+    {"navn": "Planlegger", "ikon": "🧠"}
+]
