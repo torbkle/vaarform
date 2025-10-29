34c78558cb24074381f8f42141fc5eb7673ee0e5
torbkle <torkle05553@stud.noroff.no>
2025-10-29 13:00:59 +0100

    Update meny.py

--- komponenter/meny.py
+++ komponenter/meny.py
@@ -1,1 +1,7 @@
+import streamlit as st
+
+def vis_meny():
+    st.sidebar.title("🧭 Navigasjon")
+    valg = st.sidebar.radio("Velg side:", ["Forside", "Dagens plan", "Logg", "Innstillinger"])
+    st.session_state.sidevalg = valg
 
